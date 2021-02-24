2e016c688aba5b76b8f9e562083b2a90c93915fb
Peter Sabry <petersabry84@gmail.com>
2021-02-24 22:16:10 +0200

    Final, Commented

--- Numerical Solver/univar_regressor.cpp
+++ Numerical Solver/univar_regressor.cpp
@@ -8,13 +8,17 @@ using namespace std;
 #include "Linear_system.h"
 #include "Matrix.h"
 
+////////////////////////////////////////////////////////
+// A class responsible for single variable regression //
+////////////////////////////////////////////////////////
 
 univar_regressor::univar_regressor(const double x[], const double y[], const int m, const int s)
-	:n(sizeof(x)+1)
-	, m(m)
-	, x(new double [n])
-	, y(new double [n])
-	, valid_solution(false)
+	:n(sizeof(x)+1)		//number of points
+	, m(m)				//polynomial degree
+	, x(new double [n])	//input points
+	, y(new double [n]) //target output points
+	, valid_solution(false)	//check for ill condition
+	, seidel_iterations(0)	//keeps number of iterations of gauss-seidel
 {
 }
 
@@ -23,6 +27,14 @@ univar_regressor::~univar_regressor()
 {
 }
 
+/// <summary>
+/// Fits a plynomial of a single variable
+/// x[]: Array of input points
+/// y[]: Array of the output
+/// m:	 Polynomial degree
+/// n:	 Number of points
+/// s:	 Solver choice (1. Gauss Elimination / 2. Gauss-Seidel)
+/// </summary>
 
 Matrix univar_regressor::fit(const double x[], const double y[], const int n, const int m, const int s) {
 
@@ -58,23 +70,37 @@ Matrix univar_regressor::fit(const double x[], const double y[], const int n, co
 	cout << "\nAugmented Matrix:\n" << endl;
 	cout << out << endl;
 	
+	// Initialize a linear_system with the augmented matrix [A | b]
 	Linear_system S(A, b);
+
+	// Using Gauss Elimination solver
 	if (s == 1) {
 		Matrix sol = S.solve();
 		valid_solution = S.is_valid_solution();
 		return sol;
 	}
+	//Using Gauss-Siedel solver
 	else if (s == 2) {
-		//In case of Gauss-Siedel is selected:
+		
+		//initialization vector
 		double* arr = new double[m+1];
-		//initialization
 		for (int k = 0; k < m+1; k++)
-			arr[k] = 0.1;
-		//Assume number of iterations is 2
-		Matrix sol = S.solve_until(arr, seidel_iterations, 0.01f);
+			arr[k] = 0;
+
+		Matrix sol = S.solve_until(arr, seidel_iterations, 0.001f);
+		valid_solution = S.is_valid_solution();
 		return sol;
 	}
 }
+
+/// <summary>
+/// Makes a single prediction given:
+///		xi:		 single input point
+///		coeff[]: coefficients vector (Matrix of size [n x 1]) 
+///		m:		 polynomial degree
+/// Returns a signle prediction based on the equation:
+///		y = a_0 * x^0 + a_1 * x^1 + a_2 x^2 + ..... + a_m x^m
+/// </summary>
 
 double univar_regressor::predict(const double xi, const Matrix coeff, const int m)
 {
